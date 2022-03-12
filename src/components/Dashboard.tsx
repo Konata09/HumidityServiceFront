d7ac92f5e6319e880d8d2836763797029717bd9f
Konata09 <lz9709@gmail.com>
2022-03-12 21:14:50 +0800

    add websocket

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,7 +6,6 @@ export const DashboardPage = () => {
   return (
     <div className="flex-col dashboard flex-center">
       <div className="card">
-        <div className="card-label">实时数据</div>
         <RealtimeHistory/>
       </div>
       <div className="card">
--- src/components/RealtimeHistory.tsx
+++ src/components/RealtimeHistory.tsx
@@ -1,12 +1,33 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {Dispatch, SetStateAction, useContext, useEffect, useRef, useState} from "react";
 import {Api} from "../utils/api";
 import dayjs from "dayjs";
-import {Line} from "@ant-design/plots";
 import {PipeContext} from "../Context";
 import {NodeT} from "../Types";
+import LineChart from "./LineChart";
 
 export const RealtimeHistory = (props: any) => {
   const {pipeline} = useContext(PipeContext);
+  const [data, setData] = useState<object[]>([]);
+  const [showUpdateIcon, setShowUpdateIcon] = useState<boolean>(false);
+  const ws = useRef<WebSocket | null>(null);
+  const setDataRef = useRef<Dispatch<SetStateAction<object[]>> | null>(null);
+  const dataRef = useRef<object[] | null>(null);
+  const updateIconTimer = useRef<NodeJS.Timeout | null>(null)
+
+  useEffect(() => {
+    getPipeHistory();
+    if (!ws.current || ws.current?.readyState === 3 || ws.current?.readyState === 2) {
+      establishWebsocket();
+    }
+    return () => {
+      ws.current?.close(1000, navigator.userAgent)
+    }
+  }, []);
+
+  useEffect(() => {
+    dataRef.current = data;
+    setDataRef.current = setData
+  }, [data, setData])
 
   const getNodeHistory = async (nodeId: string) => {
     let now = dayjs();
@@ -14,13 +35,11 @@ export const RealtimeHistory = (props: any) => {
     return await Api.getNodeHistory(nodeId, from.toISOString(), now.toISOString());
   }
 
-  const [data, setData] = useState<object[]>([])
-
   const getPipeHistory = async () => {
     const r = await Api.getNodes(pipeline.pipeId, false, false);
     // let nodeCount = r.data.count;
     const nodes: NodeT[] = r.data.nodes;
-    let lData: object[] = [];
+    const lData: object[] = [];
     await Promise.all(nodes.map(async (n) => {
       let r2 = await getNodeHistory(n.id);
       let points = r2.data.points;
@@ -35,57 +54,44 @@ export const RealtimeHistory = (props: any) => {
     setData(lData)
   }
 
-  useEffect(() => {
-    getPipeHistory();
-  }, []);
+  const mergeData = (d: Array<NodeT>) => {
+    if (setDataRef.current && dataRef.current) {
+      const latestData: Array<object> = [];
+      d.forEach((n) => {
+        latestData.push({
+          tag: n.tag,
+          time: n.lastTime,
+          value: n.value
+        })
+      })
+      setDataRef.current(latestData.concat(dataRef.current))
+    }
+  }
+
+  const establishWebsocket = () => {
+    ws.current = new WebSocket(`ws://${window.location.hostname}/ws`);
+    // ws.current = new WebSocket(`ws://101.34.57.204/ws`);
+    ws.current.addEventListener('message', (e) => {
+      if (updateIconTimer.current) {
+        clearTimeout(updateIconTimer.current);
+      }
+      setShowUpdateIcon(true)
+      updateIconTimer.current = setTimeout(() => setShowUpdateIcon(false), 450)
+      const r = JSON.parse(e.data)
+      mergeData(r.nodes)
+    });
+  }
+
 
   return (
-    <div id="realtime-history-canvas">
-      <Line data={data} smooth={true} theme="my-theme"
-            padding="auto" appendPadding={20} xField="time" yField="value" seriesField="tag"
-            lineStyle={{opacity: 1, shadowColor: 'rgba(255,255,255,0)', shadowBlur: 0}} autoFit={true}
-            legend={{position: "bottom"}}
-            meta={{
-              time: {
-                type: "time",
-                mask: "YYYY-MM-DD HH:mm:ss",
-                nice: true,
-                maxTickCount: 30,
-                tickMethod: ({min, max, tickCount}) => {
-                  const avg = (max - min) / tickCount;
-                  const ticks = [];
-                  for (let i = min; i <= max; i += avg) {
-                    ticks.push(i);
-                  }
-                  return ticks;
-                }
-              }
-            }}
-            tooltip={{
-              shared: true, showCrosshairs: true, showMarkers: true, offset: 50, showNil: true,
-              marker: {
-                shadowColor: 'rgba(0,0,0,0.15)',
-                shadowBlur: 2
-              },
-              domStyles: {
-                "g2-tooltip": {
-                  "border-radius": "0.5rem",
-                  "box-shadow": "0 4px 15px rgba(0, 2, 4, .1), 0 0 3px rgba(0, 2, 4, .18)",
-                  opacity: 1
-                },
-                "g2-tooltip-list": {fontSize: "1rem"},
-                "g2-tooltip-title": {fontSize: "1rem"},
-                "g2-tooltip-value": {fontWeight: 'bold'}
-              }
-            }}
-            xAxis={{
-              label: {
-                formatter(text) {
-                  return text.slice(0, 16)
-                }
-              }
-            }}
-      />
-    </div>
+    <>
+      <div className="card-label card-realtime">实时数据
+        <span className="iconfont icon-a-Arrowsvertical1-outlined"
+              style={{opacity: showUpdateIcon ? 1 : 0}}/>
+      </div>
+      <div id="realtime-history-canvas">
+        <LineChart data={data}/>
+      </div>
+    </>
   )
 }
